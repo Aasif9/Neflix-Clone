91d652df0976070017b83ff7faa5f28261c60564
Asif Ali <62379306+Aasif9@users.noreply.github.com>
2023-11-22 23:29:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,20 @@
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
+let accordian = document.getElementsByClassName("FAQ__title");
 
-// Select tab content item
-function selectItem(e) {
-	// Remove all show and border classes
-	removeBorder();
-	removeShow();
-	// Add border to current tab item
-	this.classList.add('tab-border');
-	
-	const tabContentItem = document.querySelector(`#${this.id}-content`);
-	
-	tabContentItem.classList.add('show');
+for (let i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", function () {
+    if (this.childNodes[1].classList.contains("fa-plus")) {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-times");
+    } else {
+      this.childNodes[1].classList.remove("fa-times");
+      this.childNodes[1].classList.add("fa-plus");
+    }
+
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
-
-// Remove bottom borders from all tab items
-function removeBorder() {
-	tabItems.forEach(item => {
-		item.classList.remove('tab-border');
-	});
-}
-
-// Remove show class from content items
-function removeShow() {
-	tabContentItems.forEach(item => {
-		item.classList.remove('show');
-	});
-}
-
-// Listen for tab item click
-tabItems.forEach(item => {
-	item.addEventListener('click', selectItem);
-});
